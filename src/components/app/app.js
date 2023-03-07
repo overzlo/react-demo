36a011fc74377e81900e2c6e1cbbe119aadd819e
overzlo <flashzed_969@mail.ru>
2023-03-07 20:51:10 +0600

    delete

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
 import TodoList from "../todo-list";
 import ItemStatusFilter from "../item-status-filter";
 import './app.css'
 
-const App = () => {
-
-  const todoData = [
+export default class App extends Component{
+    state = {
+    todoData:  [
     { label: 'Drink Coffee', important: false, id: 1 },
     { label: 'React Application', important: true, id: 2 },
     { label: 'Make notes from your stud', important: false, id: 3 },
-    { label: 'Do not drink alchohol', important: false, id: 4 },
-];
+    ]};
+
+    deleteItem = (id) =>{
+        this.setState(({todoData})=> {
+            const idx = todoData.findIndex((element) => element.id === id);
+            const newArray = [
+                ...todoData.slice(0,idx),
+                ...todoData.slice(idx+1)
+            ]
+            
+            return{
+                todoData: newArray
+            }
+        })
+    }
+    render(){
     return (
         <div className="todo-app">
             
@@ -21,9 +35,9 @@ const App = () => {
             <SearchPanel />
             <ItemStatusFilter />
             </div>
-            <TodoList todos={todoData}/>
+            <TodoList todos={this.state.todoData}
+                onDeleted = {this.deleteItem}/>
         </div>
     );
+    }
 }
-
-export default App;
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -25,7 +25,7 @@ export default class TodoListItem extends Component{
    
 
     render() {
-      const {label} = this.props;
+      const {label, onDeleted} = this.props;
       const {done, important} = this.state;
       let classNames = 'todo-list-item';
       if(done){
@@ -57,7 +57,7 @@ export default class TodoListItem extends Component{
 
       <button type="button"
               className="btn btn-outline-danger btn-sm float-right"
-             >
+             onClick={onDeleted}>
         <i className="fa fa-trash-o" />
       </button>
     </span>
